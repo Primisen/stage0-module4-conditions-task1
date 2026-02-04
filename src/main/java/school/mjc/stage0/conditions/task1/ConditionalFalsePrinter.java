2bfdbd116ab29a9ea06fb94bd6b2703076f589b3
Nadzeya <n.gayduk@itransition.com>
2026-02-04 18:29:03 +0100

    Practice with if conditions

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        double a = 12.4;
+        float b = 10.5f;
+        if (a < b) {
+            System.out.println(false);
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        double a = 44;
+        double b = 22;
+
+        if (a > b) {
+            System.out.println(true);
+        } else {
+            System.out.println(true);
+        }
     }
 }
